cf8b5a2b1961e8137a122f15f623c64dc627e9ed
choi jung won <chlwjddnjs56@gmail.com>
2025-11-17 06:58:08 +0900

    feat: add PolygonConfig interface and PolygonConfigImplV1 implementation - JsonReader와 ObjectMapper 의존성 주입 - GeoJSON 데이터(features)를 읽어 List<Geometry>로 변환 - Polygon/MultiPolygon 지원 가능(MultiPolygon 각 Polygon으로 분리) - 변환된 geometry 리스트를 KoreaPolygon Bean으로 등록 - @Configuration proxy를 통해 싱글톤 보장

---
+++ src/main/java/Service/RandomArea/Config/Polygon/PolygonConfig.java
@@ -0,0 +1,7 @@
+package Service.RandomArea.Config.Polygon;
+
+import Service.RandomArea.domain.Polygon.KoreaPolygon;
+
+public interface PolygonConfig {
+    public KoreaPolygon getPolygon() throws Exception;
+}
---
+++ src/main/java/Service/RandomArea/Config/Polygon/PolygonConfigImplV1.java
@@ -0,0 +1,49 @@
+package Service.RandomArea.Config.Polygon;
+
+import Service.RandomArea.Config.Json.JsonReader;
+import Service.RandomArea.domain.Polygon.KoreaPolygon;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.MultiPolygon;
+import org.locationtech.jts.io.geojson.GeoJsonReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Configuration
+@RequiredArgsConstructor
+public class PolygonConfigImplV1 implements PolygonConfig {
+    private final JsonReader jsonInputConfig;
+    private final ObjectMapper mapper; //ObjectMapper도 자동으로 스피링 빈에 등록이 된다.
+    private final GeoJsonReader reader = new GeoJsonReader();
+    @Bean
+    public KoreaPolygon getPolygon() throws Exception {
+            List<Geometry> geometries = getPolygon(jsonInputConfig.getJsonString("korea(small).json"));
+            return new KoreaPolygon(geometries);
+    }
+    private List<Geometry> getPolygon(String json) throws Exception {
+        JsonNode jsonNode = mapper.readTree(json);
+        List<Geometry> geometries = new ArrayList<>();
+        for(JsonNode feature : jsonNode.get("features")) {
+            Geometry geometry = reader.read(mapper.writeValueAsString(feature.get("geometry")));
+            if(geometry instanceof MultiPolygon) {
+                for(int i =0; i < geometry.getNumGeometries(); i++) {
+                    geometries.add(geometry.getGeometryN(i).buffer(0));
+                }
+            }
+            else{
+                geometries.add(geometry.buffer(0));
+            }
+        }
+        return geometries;
+
+
+    }
+
+}
