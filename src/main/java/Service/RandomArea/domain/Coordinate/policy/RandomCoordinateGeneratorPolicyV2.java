45eb6d70abe961355f0364a7fc72036ced4d380f
choi jung won <chlwjddnjs56@gmail.com>
2025-11-26 18:29:37 +0900

    refactor: delegate polygon lookup to PolygonService and set polygon into Coordinate

--- src/main/java/Service/RandomArea/domain/Coordinate/policy/RandomCoordinateGeneratorPolicyV2.java
+++ src/main/java/Service/RandomArea/domain/Coordinate/policy/RandomCoordinateGeneratorPolicyV2.java
@@ -2,6 +2,8 @@ package Service.RandomArea.domain.Coordinate.policy;
 
 import Service.RandomArea.Config.Polygon.PolygonConfig;
 import Service.RandomArea.domain.Coordinate.Coordinate;
+import Service.RandomArea.domain.Polygon.PolygonService;
+import Service.RandomArea.domain.Polygon.PolygonServiceImpl;
 import Service.RandomArea.exception.CustomException;
 import Service.RandomArea.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,7 @@ import org.locationtech.jts.geom.Point;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Random;
 
 @Component
@@ -19,21 +22,23 @@ import java.util.Random;
 @Primary
 @RequiredArgsConstructor
 public class RandomCoordinateGeneratorPolicyV2 implements RandomCoordinatePolicy {
-    private final PolygonConfig polygonConfig;
-    private final GeometryFactory geometryFactory = new GeometryFactory();
+/*    private final PolygonConfig polygonConfig;
+    private final GeometryFactory geometryFactory = new GeometryFactory();*/
+    private final PolygonService polygonService;
     private final Random random = new Random();
     public Coordinate generate() throws Exception {
         while (true) {
             try {
                 double x = 124.0 + random.nextDouble() * (132.0 - 124.0);
                 double y = 33.0 + random.nextDouble() * (39.0 - 33.0);
-                Point point = geometryFactory.createPoint(new org.locationtech.jts.geom.Coordinate(x, y));
-                for(Geometry polygon : polygonConfig.getPolygon().getPolygons()) {
-                    if(polygon.contains(point)) {
-                        return new Coordinate(String.valueOf(x), String.valueOf(y));
-                    }
+                List<List<Double>> polygon = polygonService.getPolygon(x, y);
+                if(polygon == null || polygon.isEmpty()){
+                    throw new CustomException(ErrorCode.MISS_COORDINATE);
                 }
-                throw new CustomException(ErrorCode.MISS_COORDINATE);
+                Coordinate coordinate = new Coordinate(String.valueOf(x), String.valueOf(y));
+                coordinate.SetPolygonCoordinates(polygon);
+                return coordinate;
+
             } catch (CustomException e) {
                 log.error(e.getMessage());
             }
