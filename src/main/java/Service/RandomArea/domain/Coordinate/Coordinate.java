b37a3f8991e553c6c6f81c680c30149d4a3ca4e8
choi jung won <chlwjddnjs56@gmail.com>
2025-11-27 00:38:30 +0900

    feat : add PolygonCoordinates List for selected random Coordinate

--- src/main/java/Service/RandomArea/domain/Coordinate/Coordinate.java
+++ src/main/java/Service/RandomArea/domain/Coordinate/Coordinate.java
@@ -3,16 +3,22 @@ package Service.RandomArea.domain.Coordinate;
 import Service.RandomArea.domain.address.Address;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class Coordinate {
     private String x;
     private String y;
     private Address address;
+    private List<List<Double>> polygonCoordinates;
     public Coordinate(String x, String y) {
         this.x = x;
         this.y = y;
     }
     public void SetAddress(Address address) {
         this.address = address;
+    }
+    public void SetPolygonCoordinates(List<List<Double>> polygonCoordinates) {
+        this.polygonCoordinates = polygonCoordinates;
     }
 }
