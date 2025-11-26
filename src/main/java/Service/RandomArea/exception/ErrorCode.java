8564616b8954dce20f64eed042e99ef399c3910f
choi jung won <chlwjddnjs56@gmail.com>
2025-11-27 00:40:29 +0900

    feat: introduce NOT_FOUND_POLYGON ErrorCode

--- src/main/java/Service/RandomArea/exception/ErrorCode.java
+++ src/main/java/Service/RandomArea/exception/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode implements CoderInterface{
     NOT_FOUND(-200, "NOT Found Address"),
     RETRY_MAX_COUNT_EXCEEDED(-201, "Retry Max Count Exceeded"),
     MISS_COORDINATE(-202, "Missing Coordinate"),
-
+    NOT_FOUND_POLYGON(-203, "Not Found Polygon"),
     SUCCESS(200, "Success");
 
     private final Integer code;
