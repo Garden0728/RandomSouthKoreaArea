b554309e39f42497eb34b7a68af1aea98138b501
choi jung won <chlwjddnjs56@gmail.com>
2025-11-28 01:17:00 +0900

    feat: add model attribute to expose Kakao JS key

--- src/main/java/Service/RandomArea/Controller/MapController.java
+++ src/main/java/Service/RandomArea/Controller/MapController.java
@@ -1,12 +1,17 @@
 package Service.RandomArea.Controller;
 
+import Service.RandomArea.Config.API.KakaoConfig;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@RequiredArgsConstructor
 @Controller
 public class MapController {
+    private final KakaoConfig kakaoConfig;
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("javascript_key", kakaoConfig.getKakaoJavascriptKey());
         return "map/Kakaomap";
     }
 
