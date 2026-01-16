b70461098fcac217fba0c5b3cb7da8a27a84ab29
choi jung won <chlwjddnjs56@gmail.com>
2026-01-17 06:28:14 +0900

    test: KakaoAddressProvider 기능 분리 테스트 고도화  - KakaoApiClient 의존성을 신규 구현체인 KakaoAddressProvider로 교체  - 기존 단일 조회 테스트를 지번및 행정동 개별 조회 검증으로 확장  - 리팩토링 후 통합 테스트 정상 작동 확인

--- src/test/java/Service/RandomArea/CoordinateServiceTest.java
+++ src/test/java/Service/RandomArea/CoordinateServiceTest.java
@@ -1,11 +1,9 @@
 package Service.RandomArea;
 
-import Service.RandomArea.API.KakaoApiClient;
-import Service.RandomArea.API.MapAPI;
+import Service.RandomArea.API.KakaoAddressProvider;
 import Service.RandomArea.Controller.Dto.APIAddressResponseDto;
 import Service.RandomArea.domain.Coordinate.Coordinate;
 import Service.RandomArea.domain.Coordinate.policy.RandomCoordinateGeneratorPolicyV2;
-import Service.RandomArea.domain.Coordinate.policy.RandomCoordinatePolicy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +14,18 @@ public class CoordinateServiceTest {
     @Autowired
     private RandomCoordinateGeneratorPolicyV2 policy;
     @Autowired
-    private KakaoApiClient kakaoApiClient;
+    private KakaoAddressProvider kakaoApiClient;
     @Test
     public void getRandomCoordinate() throws Exception {
         Coordinate coordinate = policy.generate();
         System.out.println(coordinate.getX() + " " + coordinate.getY());
         Assertions.assertNotNull(coordinate);
-        APIAddressResponseDto apiAddress = kakaoApiClient.getApiAddress(coordinate.getX(), coordinate.getY());
-        System.out.println("apiAddress = " + apiAddress.documents().get(0).address().address_name());
+        APIAddressResponseDto apiAddress = kakaoApiClient.getAddress(coordinate.getX(), coordinate.getY());
+        System.out.println(apiAddress.documents().get(0).getFullAddress());
+        APIAddressResponseDto apiAddress2 = kakaoApiClient.getRegion(coordinate.getX(), coordinate.getY());
+        System.out.println(apiAddress2.documents().get(0).getFullAddress());
     }
+
 
 
 }
