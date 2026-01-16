c8bae85fd2bc67a848e846010e458bcb5010bc24
choi jung won <chlwjddnjs56@gmail.com>
2026-01-17 06:24:00 +0900

    refactor: MapAPI 인터페이스 메서드 세분화 - 변경 : getApiAddress를 getAddress와 getRegion으로 분리 - 이유 : 주소 미등록 지역 대응을 위한 폴백 로직 구현에 필요한 메서드 정의

--- src/main/java/Service/RandomArea/API/MapAPI.java
+++ src/main/java/Service/RandomArea/API/MapAPI.java
@@ -3,5 +3,6 @@ package Service.RandomArea.API;
 import Service.RandomArea.Controller.Dto.APIAddressResponseDto;
 
 public interface MapAPI {
-    public APIAddressResponseDto getApiAddress(String x, String y);
+    public APIAddressResponseDto getAddress(String x, String y);
+    public APIAddressResponseDto getRegion(String x, String y);
 }
